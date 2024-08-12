04067702f9e23f06ccb9f6e87f8e3153eddfc445
Dudum98 <nethu209@gmail.com>
2024-08-11 23:09:25 -0400

    updated css

--- app/layout.js
+++ app/layout.js
@@ -6,12 +6,15 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Insights from the Universe",
-  
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>{metadata.title}</title>
+      </head>
       <body className={inter.className}>
         {/* Background animation */}
         <div className="area">
@@ -38,4 +41,3 @@ export default function RootLayout({ children }) {
     </html>
   );
 }
-
